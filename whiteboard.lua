cb745c00e0dbcdf78cd4fce3dc4942c5d3a5fbdf
Joseph Mansfield <sftrabbit@gmail.com>
2024-09-08 19:12:42 +0100

    Add undo
    This commit also includes a fix for scenes named "Scene".

--- whiteboard.lua
+++ whiteboard.lua
@@ -8,6 +8,7 @@ hotkey_clear    = obs.OBS_INVALID_HOTKEY_ID
 hotkey_color    = obs.OBS_INVALID_HOTKEY_ID
 hotkey_size     = obs.OBS_INVALID_HOTKEY_ID
 hotkey_erase    = obs.OBS_INVALID_HOTKEY_ID
+hotkey_undo     = obs.OBS_INVALID_HOTKEY_ID
 needs_clear     = false
 swap_color      = false
 toggle_size     = false
@@ -25,6 +26,9 @@ size_max      = 12  -- size_max must be a minimum of 2.
 
 dot_vert = obs.gs_vertbuffer_t
 line_vert = obs.gs_vertbuffer_t
+
+drawing = false
+lines = {}
 
 bit = require("bit")
 winapi = require("winapi")
@@ -48,7 +52,7 @@ source_def.create = function(source, settings)
     
     obs.vec4_from_rgba(eraser_v4, 0x00000000)
     
-    data.mouse_pos = nil
+    data.prev_mouse_pos = nil
     
     -- Create the vertices needed to draw our lines.
     update_vertices()
@@ -105,6 +109,14 @@ function script_load(settings)
     local hotkey_save_array4 = obs.obs_data_get_array(settings, "whiteboard.erasertoggle")
     obs.obs_hotkey_load(hotkey_erase, hotkey_save_array4)
     obs.obs_data_array_release(hotkey_save_array4)
+    
+    hotkey_undo = obs.obs_hotkey_register_frontend("whiteboard.undo", "Undo Whiteboard", undo)
+    if hotkey_undo == nil then
+        hotkey_undo = obs.OBS_INVALID_HOTKEY_ID
+    end
+    local hotkey_save_array5 = obs.obs_data_get_array(settings, "whiteboard.undo")
+    obs.obs_hotkey_load(hotkey_undo, hotkey_save_array5)
+    obs.obs_data_array_release(hotkey_save_array5)
 end
 
 function script_update(settings)
@@ -153,6 +165,10 @@ function script_save(settings)
     hotkey_save_array = obs.obs_hotkey_save(hotkey_erase)
     obs.obs_data_set_array(settings, "whiteboard.erasertoggle", hotkey_save_array)
     obs.obs_data_array_release(hotkey_save_array)
+    
+    hotkey_save_array = obs.obs_hotkey_save(hotkey_undo)
+    obs.obs_data_set_array(settings, "whiteboard.undo", hotkey_save_array)
+    obs.obs_data_array_release(hotkey_save_array)
 end
 
 source_def.video_tick = function(data, dt)
@@ -184,6 +200,8 @@ source_def.video_tick = function(data, dt)
         obs.obs_enter_graphics()
         obs.gs_set_render_target(data.texture, nil)
         obs.gs_clear(obs.GS_CLEAR_COLOR, obs.vec4(), 1.0, 0)
+
+        draw_lines(data, lines)
 
         obs.gs_viewport_pop()
         obs.gs_set_render_target(prev_render_target, prev_zstencil_target)
@@ -229,17 +247,11 @@ source_def.video_tick = function(data, dt)
         
     local mouse_down = winapi.GetAsyncKeyState(winapi.VK_LBUTTON)
     if mouse_down then
-        local mouse_pos = winapi.GetCursorPos()
-        if data.mouse_pos == nil then
-            data.mouse_pos = mouse_pos
-        end
-        
         local window = winapi.GetForegroundWindow()
-        window_name = winapi.InternalGetWindowText(window, nil)
-        if window_match(window_name) and 
-        (string.find(window_name, "Windowed Projector", 1, true) or
-        string.find(window_name, "Fullscreen Projector", 1, true)) then
+        if is_drawable_window(window) then
+            local mouse_pos = winapi.GetCursorPos()
             winapi.ScreenToClient(window, mouse_pos)
+
             local window_rect = winapi.GetClientRect(window)
             
             local output_aspect = data.width / data.height
@@ -258,47 +270,85 @@ source_def.video_tick = function(data, dt)
             mouse_pos.x = data.width * (mouse_pos.x - offset_x) / (window_width - offset_x*2)
             mouse_pos.y = data.height * (mouse_pos.y - offset_y) / (window_height - offset_y*2)
             
-            if valid_position(mouse_pos.x, mouse_pos.y, data.mouse_pos.x, data.mouse_pos.y, data.width, data.height) then
+            if drawing then
                 effect = obs.obs_get_base_effect(obs.OBS_EFFECT_DEFAULT)
                 if not effect then
                     return
                 end
 
-                obs.obs_enter_graphics()
-
-                local prev_render_target = obs.gs_get_render_target()
-                local prev_zstencil_target = obs.gs_get_zstencil_target()
-
-                obs.gs_set_render_target(data.texture, nil)
-                obs.gs_viewport_push()
-                obs.gs_set_viewport(0, 0, data.width, data.height)
-                obs.gs_projection_push()
-                obs.gs_ortho(0, data.width, 0, data.height, 0.0, 1.0)
-
-                obs.gs_blend_state_push()
-                obs.gs_reset_blend_state()
-                
-                -- Set the color being used (or set the eraser).
-                local solid = obs.obs_get_base_effect(obs.OBS_EFFECT_SOLID)
-                local color = obs.gs_effect_get_param_by_name(solid, "color")
-                local tech  = obs.gs_effect_get_technique(solid, "Solid")
-
-                if eraser then
-                    obs.gs_blend_function(obs.GS_BLEND_SRCALPHA, obs.GS_BLEND_SRCALPHA)
-                    obs.gs_effect_set_vec4(color, eraser_v4)
-                else
-                    local color_v4 = obs.vec4()
-                    obs.vec4_from_rgba(color_v4, color_array[color_index])
-                    obs.gs_effect_set_vec4(color, color_v4)
+                local new_segment = { erase = lines[#lines].erase, points = {
+                    { x = data.prev_mouse_pos.x, y = data.prev_mouse_pos.y },
+                    { x = mouse_pos.x, y = mouse_pos.y }
+                }}
+                draw_lines(data, { new_segment })
+                table.insert(lines[#lines].points, { x = mouse_pos.x, y = mouse_pos.y })
+            else
+                if valid_position(mouse_pos.x, mouse_pos.y, data.width, data.height) then
+                    table.insert(lines, { erase = eraser, points = {{ x = mouse_pos.x, y = mouse_pos.y }}})
+                    drawing = true
                 end
+            end
+
+            data.prev_mouse_pos = mouse_pos
+        end
+    else
+        if data.prev_mouse_pos then
+            data.prev_mouse_pos = nil
+            drawing = false
+        end
+    end
+end
+
+function is_drawable_window(window)
+    window_name = winapi.InternalGetWindowText(window, nil)
+
+    return window_match(window_name) and
+        (string.find(window_name, "Windowed Projector", 1, true) or
+        string.find(window_name, "Fullscreen Projector", 1, true))
+end
+
+function draw_lines(data, lines)
+    obs.obs_enter_graphics()
+
+    local prev_render_target = obs.gs_get_render_target()
+    local prev_zstencil_target = obs.gs_get_zstencil_target()
+
+    obs.gs_set_render_target(data.texture, nil)
+    obs.gs_viewport_push()
+    obs.gs_set_viewport(0, 0, data.width, data.height)
+    obs.gs_projection_push()
+    obs.gs_ortho(0, data.width, 0, data.height, 0.0, 1.0)
+
+    for _, line in ipairs(lines) do
+        if #(line.points) > 1 then
+            obs.gs_blend_state_push()
+            obs.gs_reset_blend_state()
             
-                obs.gs_technique_begin(tech)
-                obs.gs_technique_begin_pass(tech, 0)
+            -- Set the color being used (or set the eraser).
+            local solid = obs.obs_get_base_effect(obs.OBS_EFFECT_SOLID)
+            local color = obs.gs_effect_get_param_by_name(solid, "color")
+            local tech  = obs.gs_effect_get_technique(solid, "Solid")
+
+            if line.erase then
+                obs.gs_blend_function(obs.GS_BLEND_SRCALPHA, obs.GS_BLEND_SRCALPHA)
+                obs.gs_effect_set_vec4(color, eraser_v4)
+            else
+                local color_v4 = obs.vec4()
+                obs.vec4_from_rgba(color_v4, color_array[color_index])
+                obs.gs_effect_set_vec4(color, color_v4)
+            end
+
+            obs.gs_technique_begin(tech)
+            obs.gs_technique_begin_pass(tech, 0)
+
+            for i=1, (#(line.points) - 1) do
+                local start_pos = line.points[i]
+                local end_pos = line.points[i+1]
 
                 -- Calculate distance mouse has traveled since our
                 -- last update.
-                local dx = mouse_pos.x - data.mouse_pos.x
-                local dy = mouse_pos.y - data.mouse_pos.y
+                local dx = end_pos.x - start_pos.x
+                local dy = end_pos.y - start_pos.y
                 local len = math.sqrt(dx*dx + dy*dy)
                 local angle = math.atan2(dy, dx)
                 
@@ -306,7 +356,7 @@ source_def.video_tick = function(data, dt)
                 -- start of the line (start cap).
                 obs.gs_matrix_push()
                 obs.gs_matrix_identity()
-                obs.gs_matrix_translate3f(data.mouse_pos.x, data.mouse_pos.y, 0)
+                obs.gs_matrix_translate3f(start_pos.x, start_pos.y, 0)
 
                 obs.gs_matrix_push()
                 obs.gs_matrix_translate3f(-size, -size, 0)
@@ -326,35 +376,30 @@ source_def.video_tick = function(data, dt)
                 obs.gs_load_vertexbuffer(line_vert)
                 obs.gs_draw(obs.GS_TRIS, 0, 0)
 
-
                 -- Perform matrix transforms for the dot at the end
                 -- of the line (end cap).
                 obs.gs_matrix_identity()
-                obs.gs_matrix_translate3f(mouse_pos.x, mouse_pos.y, 0)
+                obs.gs_matrix_translate3f(end_pos.x, end_pos.y, 0)
                 obs.gs_matrix_translate3f(-size, -size, 0)
                 obs.gs_load_vertexbuffer(dot_vert)
                 obs.gs_draw(obs.GS_LINESTRIP, 0, 0)
 
-                -- Done drawing line, restore everything.
-                obs.gs_technique_end_pass(tech)
-                obs.gs_technique_end(tech)
-
                 obs.gs_matrix_pop()
-
-                obs.gs_projection_pop()
-                obs.gs_viewport_pop()
-                obs.gs_blend_state_pop()
-                obs.gs_set_render_target(prev_render_target, prev_zstencil_target)
-
-                obs.obs_leave_graphics()
-                
             end
 
-            data.mouse_pos = mouse_pos
+            -- Done drawing line, restore everything.
+            obs.gs_technique_end_pass(tech)
+            obs.gs_technique_end(tech)
+
+            obs.gs_blend_state_pop()
         end
-    else
-        data.mouse_pos = nil
     end
+
+    obs.gs_projection_pop()
+    obs.gs_viewport_pop()
+    obs.gs_set_render_target(prev_render_target, prev_zstencil_target)
+
+    obs.obs_leave_graphics()
 end
 
 -- Check whether current foreground window is relevant to us.
@@ -379,12 +424,14 @@ function window_match(window_name)
 
     -- Check that the currently selected projector matches one
     -- of the ones listed above.
-    for name_index = 1, #valid_names do        
-        local start, finish = string.find(window_name, valid_names[name_index], 1, true)
-        if start and string.len(window_name) == finish then
+    for name_index = 1, #valid_names do
+        local valid_name = valid_names[name_index]
+        local window_name_suffix = string.sub(window_name, -string.len(valid_name) - 1)
+        if window_name_suffix == (" " .. valid_name) then
             return true
         end
     end
+
     return false
 end
 
@@ -394,6 +441,10 @@ function update_vertices()
     -- LINE VERTICES
     -- Create vertices for line of given width (user-defined 'size').
     -- These vertices are for two triangles that make up each line.
+    if line_vert then
+        obs.gs_vertexbuffer_destroy(line_vert)
+    end
+
     obs.gs_render_start(true)
     local width = size * 2
     obs.gs_vertex2f(0, 0)
@@ -405,10 +456,13 @@ function update_vertices()
     
     line_vert = obs.gs_render_save()
     
-    
     -- DOT VERTICES
     -- Create vertices for a dot (filled circle) of specified width,
     -- which is used to round off the ends of the lines.
+    if dot_vert then
+        obs.gs_vertexbuffer_destroy(dot_vert)
+    end
+
     local decision = 0
     local xcoord = 0
      -- set initial ycoord to radius of circle (user-defined 'size')
@@ -447,27 +501,27 @@ function update_vertices()
     obs.obs_leave_graphics()
 end
 
-function valid_position(cur_x, cur_y, prev_x, prev_y, width, height)
+function valid_position(cur_x, cur_y, width, height)
     -- If the mouse is within the boundaries of the screen, or was
     -- previously within the boundaries of the screen, it is a valid
     -- position to draw a line to.
-    if ((cur_x >= 0 and cur_x < width and cur_y >= 0 and cur_y < height)
-        or(prev_x >= 0 and prev_x < width and prev_y >= 0 and prev_y < height)) then
+    if (cur_x >= 0 and cur_x < width and cur_y >= 0 and cur_y < height) then
         return true
     end    
     return false
 end
 
 function clear(pressed)
-    if not pressed then
+    if not pressed or not is_drawable_window(winapi.GetForegroundWindow()) then
         return
     end
 
+    clear_table(lines)
     needs_clear = true
 end
 
 function colorswap(pressed)
-    if not pressed then
+    if not pressed or not is_drawable_window(winapi.GetForegroundWindow()) then
         return
     end
 
@@ -475,7 +529,7 @@ function colorswap(pressed)
 end
 
 function sizetoggle(pressed)
-    if not pressed then
+    if not pressed or not is_drawable_window(winapi.GetForegroundWindow()) then
         return
     end
 
@@ -483,15 +537,32 @@ function sizetoggle(pressed)
 end
 
 function erasertoggle(pressed)
-    if not pressed then
+    if not pressed or not is_drawable_window(winapi.GetForegroundWindow()) then
         return
     end
 
     eraser = not eraser
 end
 
-function clear_button()
+function undo(pressed)
+    if not pressed or not is_drawable_window(winapi.GetForegroundWindow()) then
+        return
+    end
+
+    if #lines > 0 then
+        table.remove(lines, #lines)
+    end
+
     needs_clear = true
+end
+
+function clear_button()
+    clear_table(lines)
+    needs_clear = true
+end
+
+function clear_table(tab)
+    for k, _ in pairs(tab) do tab[k] = nil end
 end
 
 function image_source_load(image, file)
@@ -543,11 +614,11 @@ source_def.video_render = function(data, effect)
 end
 
 source_def.get_width = function(data)
-    return 0
+    return 1920
 end
 
 source_def.get_height = function(data)
-    return 0
+    return 1080
 end
 
 -- When source is active, get the currently displayed scene's name and
@@ -581,10 +652,10 @@ function script_properties()
     
     obs.obs_properties_add_button(properties, "clear", "Clear Whiteboard", clear_button)
     
-    obs.obs_data_set_int(settings, "color", 5)
-    obs.obs_data_set_int(settings, "size", 2)
+    -- obs.obs_data_set_int(settings, "color", 5)
+    -- obs.obs_data_set_int(settings, "size", 2)
     
-    obs.obs_data_set_bool(settings, "eraser", false)
+    -- obs.obs_data_set_bool(settings, "eraser", false)
     
     return properties
 end
@@ -611,5 +682,17 @@ Hotkeys can be set to toggle color, size, and eraser. An additional hotkey can b
 The settings on this page will unfortunately not update in real-time when using hotkeys, due to limitations with OBS's script properties.]==]
 end
 
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
 
 obs.obs_register_source(source_def)
